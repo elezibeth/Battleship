5e5d3e63849760e5e9a134efbb6507e98658cbbd
Elezibeth <elezibethmorgan@my.unt.edu>
2021-02-15 17:23:28 -0600

    New carrier : marinevessel class created.

---
+++ Battleship/Battleship/Carrier.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Carrier : MarineVessel
+    {
+
+        public int length;
+
+        public char[,] carrier;
+
+        public Destroyer()
+
+        {
+            length = 5;
+
+
+            type = "carrierr";
+            CreateVessel();
+
+
+
+
+        }
+
+
+        public void CreateVessel()
+        {
+            {
+                char[,] carrier = new char[2, 1];
+
+
+            }
+
+        }
+    }
+}
