8973b3cd903a1d36ee162416ab4d866bad2adcaf
Elezibeth <elezibethmorgan@my.unt.edu>
2021-02-15 18:55:43 -0600

    EnemyFleet : Gameboard created.

---
+++ Battleship/Battleship/EnemyFleet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class EnemyFleet : GameBoard
+    {
+        public List<MarineVessel> enemyLocations;
+
+        public EnemyFleet()
+        {
+
+        }
+
+        public void ChooseLocation()
+        {
+
+        }
+        public void Miss()
+        {
+
+        }
+        public void Hit()
+        {
+
+        }
+        public void DisplayGameBoard()
+        {
+
+        }
+    }
+}
